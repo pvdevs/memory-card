97cd11326b5d2e5c11bdb33050db747a9ffeea75
Paulo Victor <paulovictor.devs@gmail.com>
2024-01-02 21:25:16 -0300

    add cards object and states

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
 import { useState } from 'react';
 
 export default function App() {
+  // Game Logic - Cards
+  const defaultCards = [
+    {
+      nome: 'paulo',
+      isClicked: false,
+      id: 214,
+    },
+    {
+      nome: 'viviane',
+      isClicked: false,
+      id: 455,
+    },
+    {
+      nome: 'brian',
+      isClicked: false,
+      id: 995,
+    },
+    {
+      nome: 'bea',
+      isClicked: false,
+      id: 222,
+    },
+  ];
+
+  const [cards, setCards] = useState(defaultCards);
+
+  function clickCard(id) {
+    setCards(
+      cards.map((card) => {
+        if (card.id === id) {
+          return { ...card, isClicked: true };
+        } else {
+          return card;
+        }
+      })
+    );
+  }
+
   // Game Logic - Scores
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
@@ -19,6 +57,9 @@ export default function App() {
     <div>
       <button onClick={() => checkIfBestScore(1)}>EE</button>
       {bestScore}
+
+      <button onClick={() => clickCard(214)}>Cards!!!</button>
+      {console.log(cards)}
     </div>
   );
 }
